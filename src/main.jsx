1d18ddde6bc53ac9eb2a7818561231249c19a214
Naheed Miah <naheed.miah@gmail.com>
2026-03-15 04:11:15 +0000

    Correct HashRouter import and push final fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { hashRouter as HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
